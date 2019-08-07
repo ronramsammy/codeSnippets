5383da114f4d03ec0508456c424e9fcf69d82f27
Ron Ramsammy <ronramsammy@gmail.com>
2019-08-07 13:18:58 -0400

    added recursive binary search base

--- binarySearchRecursive.py
+++ binarySearchRecursive.py
@@ -1,38 +1,37 @@
 # Returns index of x in arr if present, else -1 
-def _binarySearchR (arr, l, r, x): 
-  
-    # Check base case 
-    if r >= l: 
-  
-        mid = l + (r - l)//2
-  
-        # If element is present at the middle itself 
-        if arr[mid] == x: 
-            return mid 
-          
-        # If element is smaller than mid, then it can only 
-        # be present in left subarray 
-        elif arr[mid] > x: 
-            return _binarySearchR(arr, l, mid-1, x) 
-  
-        # Else the element can only be present in right subarray 
-        else: 
-            return _binarySearchR(arr, mid+1, r, x) 
-  
-    else: 
-        # Element is not present in the array 
-        return -1
 
 def binarySearchR(arr,x):
-    res =_binarySearchR(arr,0,len(arr)-1,x)
+    res =  _binarySearchR(arr,0,len(arr)-1,x)
+    print(f"res={res}")
     return res
+
+
+def _binarySearchR(arr, left, right, x ):
+    print (f"checking array {arr[left:right]}")
+    if( right >= left ):
+        mid = left + (right-left)//2
+        print(f"mid = {mid}")
+        
+        if(arr[mid]==x):
+            print(f"returning index(mid) = {mid}")
+            return mid
+        elif( x<arr[mid]):
+            return _binarySearchR(arr, left, mid-1, x)
+        else:
+            return _binarySearchR(arr, mid+1, right, x)
+            
+    else:
+        return -1
+        
+
+
 
 # Test array 
 arr = [ 2, 3, 4, 10, 40 ] 
-x = 4
+x = 99
   
 # Function call 
-result = binarySearchR(arr, x) 
+result = _binarySearchR(arr,0, len(arr)-1, x) 
   
 if result != -1: 
     print ("Element is present at index %d" % result )
