483a700e801b619246e6d2287550260e9b7c83ef
Ron Ramsammy <ronramsammy@gmail.com>
2019-08-08 06:38:53 -0400

    binary tree example

--- binarySearchRecursive.py
+++ binarySearchRecursive.py
@@ -1,39 +1,8 @@
-# Returns index of x in arr if present, else -1 
-
-def binarySearchR(arr,x):
-    res =  _binarySearchR(arr,0,len(arr)-1,x)
-    print(f"res={res}")
-    return res
-
-
-def _binarySearchR(arr, left, right, x ):
-    print (f"checking array {arr[left:right]}")
-    if( right >= left ):
-        mid = left + (right-left)//2
-        print(f"mid = {mid}")
-        
-        if(arr[mid]==x):
-            print(f"returning index(mid) = {mid}")
-            return mid
-        elif( x<arr[mid]):
-            return _binarySearchR(arr, left, mid-1, x)
-        else:
-            return _binarySearchR(arr, mid+1, right, x)
-            
-    else:
-        return -1
+class Node:
+    def __init__(self, data):
+        self.left = None
+        self.right = None
+        self.data = data
         
 
-
-
-# Test array 
-arr = [ 2, 3, 4, 10, 40 ] 
-x = 99
-  
-# Function call 
-result = _binarySearchR(arr,0, len(arr)-1, x) 
-  
-if result != -1: 
-    print ("Element is present at index %d" % result )
-else: 
-    print ("Element is not present in array"    )
+root = Node(8)
---
+++ binaryTree.py
@@ -0,0 +1,165 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug  7 14:11:15 2019
+
+@author: User
+"""
+
+class Node:
+    """
+    Tree node: left and right child + data which can be any object
+    """
+    def __init__(self, data):
+        """
+        Node constructor
+
+        @param data node data object
+        """
+        self.left = None
+        self.right = None
+        self.data = data
+
+
+    def insert(self, data):
+        """
+        Insert new node with data
+
+        @param data node data object to insert
+        """
+        if self.data:
+            if data < self.data:
+                if self.left is None:
+                    self.left = Node(data)
+                else:
+                    self.left.insert(data)
+            elif data > self.data:
+                if self.right is None:
+                    self.right = Node(data)
+                else:
+                    self.right.insert(data)
+        else:
+            self.data = data
+            
+            
+            
+    def lookup(self, data, parent=None):
+        """
+        Lookup node containing data
+
+        @param data node data object to look up
+        @param parent node's parent
+        @returns node and node's parent if found or None, None
+        """
+        if data < self.data:
+            if self.left is None:
+                return None, None
+            return self.left.lookup(data, self)
+        elif data > self.data:
+            if self.right is None:
+                return None, None
+            return self.right.lookup(data, self)
+        else:
+            return self, parent            
+            
+
+    def print_tree(self):
+        """
+        Print tree content inorder
+        """
+        if self.left:
+            self.left.print_tree()
+        print (self.data)
+        if self.right:
+            self.right.print_tree()            
+
+
+
+    def children_count(self):
+        """
+        Returns the number of children
+
+        @returns number of children: 0, 1, 2
+        """
+        cnt = 0
+        if self.left:
+            cnt += 1
+        if self.right:
+            cnt += 1
+        return cnt
+    
+    
+    
+    def delete(self, data):
+        """
+        Delete node containing data
+
+        @param data node's content to delete
+        """
+        # get node containing data
+        node, parent = self.lookup(data)
+        if node is not None:
+            children_count = node.children_count()
+    
+        if children_count == 0:
+            # if node has no children, just remove it
+            if parent:
+                if parent.left is node:
+                    parent.left = None
+                else:
+                    parent.right = None
+                del node
+            else:
+                self.data = None 
+                    
+        elif children_count == 1:
+            # if node has 1 child
+            # replace node with its child
+            if node.left:
+                n = node.left
+            else:
+                n = node.right
+            if parent:
+                if parent.left is node:
+                    parent.left = n
+                else:
+                    parent.right = n
+                del node
+            else:
+                self.left = n.left
+                self.right = n.right
+                self.data = n.data
+                    
+        else:
+            # if node has 2 children
+            # find its successor
+            parent = node
+            successor = node.right
+            while successor.left:
+                parent = successor
+                successor = successor.left
+            # replace node data by its successor data
+            node.data = successor.data
+            # fix successor's parent's child
+            if parent.left == successor:
+                parent.left = successor.right
+            else:
+                parent.right = successor.right
+        
+
+root = Node(8)
+
+root.insert(3)
+root.insert(10)
+root.insert(1)
+
+root.insert(6)
+root.insert(4)
+root.insert(7)
+root.insert(14)
+root.insert(13)    
+
+name,parent = root.lookup(6)
+
+root.print_tree()
+
+root.delete(3)
